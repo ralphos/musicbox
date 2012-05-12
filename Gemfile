ca6a4a941b057f35dfc65e21d4479e51ceff0055
Ralphos <wintle.ralph@gmail.com>
2012-05-11 19:51:12 -0500

    Almost got authentication working

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '3.2.3'
 
 gem 'thin'
 
+gem 'omniauth-twitter'
+
 group :development do
   gem 'sqlite3'
 end
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, "E6G5gEsjjJkdtHrEeEPsA", "gbJCNFyaO08iAMGfxE0QpPE7zMxoJH2xGpw8f1Gmo" 
+end
