6fe6b5d7a323c752aba906157693e608faaf4a9f
Ralphos <wintle.ralph@gmail.com>
2012-05-11 19:07:15 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Musicbox::Application.config.secret_token = '69e70a6e3857a64ee5645dbac9e41d90f39f375d86697751016bb65cde9766fd7a3dc7489fc2b6e73aff42c7ab0f64604d9dbcd85a1681ecd1c00e9cf67dd2f2'
