f5c5122b28ef68a6c003fc632fa80dae572c733e
Tolulope Bukola <tbukola@gmail.com>
2012-05-11 21:51:43 -0500

    User models and manual addition of songs

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Musicbox::Application.routes.draw do
-
   root :to => 'pages#landing'
 
+  resources :songs
+  
   match '/auth/:provider/callback' => 'sessions#create'
   match '/auth/failure' => 'sessions#failure'
 
   match '/signout' => 'sessions#destroy', :as => :signout
   match '/signin' => 'sessions#new', :as => :signin
 
-  match '/songs' =>'songs#index'
-
   post '/get-song' => 'pages#get-song'
   
+  match '/songs/new'=>'songs#new', :as=> :new_song
+  
+
 end
